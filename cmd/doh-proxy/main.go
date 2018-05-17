5427cc15a7f871ab5489f815576105998762c86b
Eric Yan <github@ericyan.me>
2018-05-17 21:01:17 +0800

    Implement a simple DOH proxy

---
+++ cmd/doh-proxy/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/ericyan/doh"
+)
+
+var (
+	bind     = flag.String("bind", "127.0.0.1", "interface to bind")
+	port     = flag.Int("port", 8053, "port to run on")
+	upstream = flag.String("upstream", "8.8.8.8:53", "upstream to use")
+)
+
+func main() {
+	flag.Parse()
+
+	srv := &http.Server{
+		Addr:    *bind + ":" + strconv.Itoa(*port),
+		Handler: http.HandlerFunc(doh.HandleWireFormat(*upstream)),
+	}
+
+	log.Printf("Listening on %s:%d...\n", *bind, *port)
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("Failed to start HTTP server: %s\n", err.Error())
+	}
+}
